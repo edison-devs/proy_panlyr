754e067d07b3ca22f107e1c7bcf6b6130215dbfa
edison6009 <edison09universidad@gmail.com>
2025-09-16 17:38:32 -0400

    cambios a configs

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 # URL de app Panlyr
+from django.contrib import admin
 from django.urls import path, include
 from core.views import *
 
-
-
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', render_home, name='home'),
     path('home1', render_home1, name='home1'),
 ]
